a62c1ca386c4656135851fa56eb892189a6f4250
Veronica Harris <veronica.n.harris92@gmail.com>
2022-03-29 13:27:52 -0400

    more psuedocoding for the concatination process of the function in this project

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,7 @@ function userSelected () {
 }
 
 // Need an array to randomize the elements selected from the user options
-function randomSelected(choice) {
+function randomSelected(randomizedArr) {
   // use math.floor to select random choice within the choice array
   // choose random characters in the array
   // return the random element
@@ -27,7 +27,28 @@ function randomSelected(choice) {
 
 // Function to generate the password from user input
 function generatePassword() {
-  
+  // create an empty variable to store the concatenated password
+  // create an empty variable holding all possible password choices
+  // create an empty variable to hold the chosen characters
+
+  // error handling to see if the object with your choices exist or not
+
+  // Conditional statements are needed to add if the user has chosen special characters into an array of possible characters and must push new random characters to chosen array
+  if (condition) {
+    
+  }
+
+  // iterate over the password length from the choice made (obj), selecting random indexes from the array of possible characters and puts them into a result variable
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  }
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+  }
+
+  // combine the results and send them to the writePassword function to present on the page
 }
 
 // Assignment Code
