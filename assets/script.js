a674ea96b59ffe51143b1a4a82a8bd23c6551d7c
Veronica Harris <veronica.n.harris92@gmail.com>
2022-03-29 11:48:41 -0400

    began adding arrays and named the first password options function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,17 @@
 // Arrays for special characters, lowercase letters, uppercase letters, and numbers
+const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
 
+const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Function to get the password options selected by the user
+
+function userSelected (){
+  
+}
 
 // Need an array to randomize the elements selected from the user options
 
