1616d38a619af6461bbb740a7e7d756de39de1ca
Veronica Harris <veronica.n.harris92@gmail.com>
2022-03-29 11:24:44 -0400

    added psuedocode to the script.js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
+// Arrays for special characters, lowercase letters, uppercase letters, and numbers
+
+
+// Function to get the password options selected by the user
+
+// Need an array to randomize the elements selected from the user options
+
+// Function to generate the password from user input
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
